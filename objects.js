8abab869a9ef3e72bba97544a8437b2c8910cb47
Andrew K <ajkloecker@outlook.com>
2017-10-19 21:37:34 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,10 +2,10 @@ var playlist = new Object();
 playlist.artist = "song"
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist[artistName] = songTitle
+  playlist.artistName = songTitle
   return playlist
 }
 
 function removeFromPlaylist(playlist, artistName){
-  delete playlist.[artistName]
+  delete playlist.artistName
 }

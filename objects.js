3704416bb4ed7a84b2b4e53ba8813bb73a3db60d
Andrew K <ajkloecker@outlook.com>
2017-10-19 21:08:10 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {stand: "by me"}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.[artistName]
+}
